a47c4809ad188e0ad0b7c2829e6c2c59660d820a
faricandrevano <faric26andrevano@gmail.com>
2024-08-04 09:04:11 +0700

    add more assets

--- lib/presentation/constants/styles.dart
+++ lib/presentation/constants/styles.dart
@@ -9,3 +9,21 @@ TextStyle headingM =
     GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 36);
 TextStyle headingS =
     GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 24);
+
+TextStyle bodyXXL =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 20);
+TextStyle bodyXL =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 18);
+TextStyle bodyL =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 16);
+TextStyle bodyM =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 14);
+TextStyle bodyS =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 12);
+TextStyle bodyXS =
+    GoogleFonts.plusJakartaSans(color: const Color(0xff101623), fontSize: 12);
+
+FontWeight bold = FontWeight.w700;
+FontWeight semiBold = FontWeight.w600;
+FontWeight medium = FontWeight.w500;
+FontWeight regular = FontWeight.w400;
